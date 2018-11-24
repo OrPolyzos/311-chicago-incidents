4c9376a1853957b195e3f7333df1f9091f9d9113
OrPolyzos <orestes.polyzos@gmail.com>
2018-11-24 12:46:11 +0200

    Creates base project

---
+++ src/main/java/com/uoa/di/csr/Chicago311ServiceRequestsApplication.java
@@ -0,0 +1,12 @@
+package com.uoa.di.csr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chicago311ServiceRequestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chicago311ServiceRequestsApplication.class, args);
+    }
+}
